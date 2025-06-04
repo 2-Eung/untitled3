deecdf2887aad23a9a67cc00baf3a75f4b76d56b
2-Eung <eung4256@gmail.com>
2025-06-04 14:22:01 +0900

    for for 99dan

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
-        for(int i = 0; i < 10; i++) {
-
-            if (i%3 == 2) {
-                System.out.println((i + 1));
-           }
+        for(int i = 1; i <= 9; i++) {
+            System.out.println("구구단 " + i + "단" );
+            for(int j = 1; j <= 9; j++) {
+                System.out.println(i + " x " + j + " = " + (i * j));
+            }
+            System.out.println(" ");
         }
     }
 }
