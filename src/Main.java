34f3d0072ac0dc762956ff81f902992801502b99
2-Eung <eung4256@gmail.com>
2025-06-04 10:11:08 +0900

    && || !

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int age = 20;
+        boolean hasID = true;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        if((age >= 18) && hasID) {
+            System.out.println("접속가능");
+        }
+
+        boolean isWeekend = false;
+        boolean isHoliday = true;
+
+        if (isWeekend || isHoliday) {
+            System.out.println("쉴수있습니다.");
+        }
+
+        boolean isRaining = true;
+        if(!isRaining) {
+            System.out.println("그냥 나가.");
+        } else {
+            System.out.println("우산 챙겨.");
         }
     }
 }
